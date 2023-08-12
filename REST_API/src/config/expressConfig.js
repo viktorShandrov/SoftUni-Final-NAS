fc6a1b6e4fd66d7908a7d6e2300edb29d053f5b8
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-08-12 19:18:34 +0300

    .

--- REST_API/src/config/expressConfig.js
+++ REST_API/src/config/expressConfig.js
@@ -11,7 +11,7 @@ exports.expressConfig = (app) => {
   // app.use(bodyParser.json())
 
   const corsMiddleware = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://theconfederacy.site');
+    res.setHeader('Access-Control-Allow-Origin', 'https://viktorshandrov.github.io');
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
