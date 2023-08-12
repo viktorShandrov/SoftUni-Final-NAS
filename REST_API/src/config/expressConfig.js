e8464c33182b6717770a88a108f7605805961afc
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-08-12 16:41:44 +0300

    adding http://theconfederacy.site as origin

--- REST_API/src/config/expressConfig.js
+++ REST_API/src/config/expressConfig.js
@@ -4,17 +4,18 @@ const cors = require('cors');
 const { auth } = require("../utils/authentication");
 const bodyParser = require("body-parser");
 
-exports.expressConfig=(app)=>{
-  app.use(express.urlencoded({extended: true,limit:"1gb"}))
-  app.use(express.json({limit:"1gb"}))
+exports.expressConfig = (app) => {
+  app.use(express.urlencoded({ extended: true, limit: "1gb" }))
+  app.use(express.json({ limit: "1gb" }))
   // app.use(bodyParser.urlencoded({extended:true}))
   // app.use(bodyParser.json())
-  
+
   const corsMiddleware = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://theconfederacy.site');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
+
     next();
   };
   app.use(corsMiddleware)
