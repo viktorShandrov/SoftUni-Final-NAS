4235a9cc6fada49c51d0b041463c99ab22119538
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-09-13 17:47:46 +0300

    .

--- ANGULAR/src/app/core/router/admin-guard.ts
+++ ANGULAR/src/app/core/router/admin-guard.ts
@@ -3,6 +3,7 @@ import { CanActivate, Router } from '@angular/router';
 import {ToastrService} from "ngx-toastr";
 import {constants} from "../../shared/constants";
 import {HttpService} from "../../shared/services/http.service";
+import {map, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -16,19 +17,15 @@ export class AuthGuard implements CanActivate {
 
   ) {}
 
-  canActivate(): boolean {
-    this.HttpService.httpGETRequest("api/users/isAdmin").subscribe(
-      (res)=>{
-      return true;
-
-      }
-    )
-    if (token&&rootId) {
-    } else {
-      this.ToastrService.warning("you are not admin","Unable to proceed",constants.toastrOptions)
-      this.router.navigate(['/users', { outlets: { 'users-view': 'login' } }]);
-      return false;
-    }
+  canActivate(): Observable<boolean> {
+    return this.HttpService.httpGETRequest("api/users/isAdmin").pipe(
+      map((res:any) => {
+        return !!res.isAdmin;
+      })
+    );
   }
+
 }
 
+      // this.ToastrService.warning("you are not admin","Unable to proceed",constants.toastrOptions)
+      // this.router.navigate(['/users', { outlets: { 'users-view': 'login' } }]);
