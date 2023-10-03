8f7b22c9c79ba8ab561e7962aba02af11132aa58
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-03 19:00:04 +0300

    plans responsive design

--- ANGULAR/src/app/features/public/components/plans-view/plans-view.component.ts
+++ ANGULAR/src/app/features/public/components/plans-view/plans-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-plans-view',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./plans-view.component.css']
 })
 export class PlansViewComponent {
+constructor(
+  private element:ElementRef,
+  private Renderer2:Renderer2,
 
+) {
+  this.Renderer2.setStyle(this.element.nativeElement,"display","flex")
+  this.Renderer2.setStyle(this.element.nativeElement,"flex-direction","column")
+  this.Renderer2.setStyle(this.element.nativeElement,"height","100% ")
+}
 }
