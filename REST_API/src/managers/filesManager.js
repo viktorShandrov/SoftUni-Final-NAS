758eaa682df50a96a2b6ac97b0131dc02a1eb704
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-16 20:31:32 +0300

    download file and folders

--- REST_API/src/managers/filesManager.js
+++ REST_API/src/managers/filesManager.js
@@ -2,16 +2,21 @@ const folderModel = require("../models/folderModel")
 const fileModel = require("../models/fileModel")
 const rootModel = require("../models/rootModel")
 const userModel = require("../models/userModel")
+const fileContainerModel = require("../models/fileContainerModel")
 const mongoose = require("mongoose")
 const uuid = require("uuid")
 const { ObjectId } = require('mongodb');
+const archiver = require("archiver");
 
 
 
 
 exports.createRoot = async (ownerId) => {
-    const root = await rootModel.create({ownerId, dirComponents: [], autorised: [ownerId], isPublic: false,storageVolume:10000000000,usedStorage:0 })
-    return root._id
+    return await rootModel.create({ownerId, dirComponents: [], autorised: [ownerId], isPublic: false,storageVolume:10000000000,usedStorage:0 })
+}
+exports.createFileContainer = async (type,ownerId,rootId,storageVolumeGB)=>{
+    const fileContainer = await fileContainerModel.create({type,ownerId,rootId,fileComponents:[],storageVolume:storageVolumeGB*1000000000})
+    return fileContainer._id
 }
 exports.getFileInfo = (fileId) => {
     return fileModel.findById(fileId)
@@ -112,30 +117,70 @@ exports.getDetails =async (id,elementType)=>{
         return folderModel.findById(id).populate("autorised")
     }
 }
-exports.getFolder = async (id,userId) => {
-    const folder = await folderModel.findById(id).populate("dirComponents").populate("fileComponents") || await rootModel.findById(id).populate("dirComponents").populate("fileComponents")
+exports.getFolder = async (id,userId,rootId) => {
+    const root = await rootModel.findById(rootId).populate("dirComponents").populate("fileComponents").populate("paidFileContainer")
+    let folder = await folderModel.findById(id).populate("dirComponents").populate("fileComponents")
+
+    if(!folder) folder = root
+
+    //to not disturb the actual records
+    folder = folder.toObject()
+
     if(!folder.isPublic){
-        if(!folder.autorised.includes(userId)){
-             throw new Error("You are not autorised")
+        if(!folder.autorised.some(id=>id.equals(userId))){
+             throw new Error("You are not authorized")
         }
-    
+
+        checkForLockedFiles(root.paidFileContainer,folder)
+
         return folder
     
     }else{
+        checkForLockedFiles(root.paidFileContainer,folder)
+
         return folder
     }
 
 
 }
+function checkForLockedFiles(fileContainer,folder){
+    if(fileContainer.isLocked){
+        for (const file of folder.fileComponents) {
+            if(fileContainer.fileComponents.includes(file._id)){
+                file.isLocked = true
+            }
+        }
+
+    }
+}
 // exports.getFilesFromSharedFolder = async (folderId) => {
 //     const folder = await folderModel.findById(folderId).populate("fileComponents") 
 //     return folder.fileComponents
 // }
+async function putInFileContainer(file,root){
+    root = await root.populate("freeFileContainer paidFileContainer")
+    console.log(root)
+    if(root.freeFileContainer.storageVolume>=file.length){
+        root.freeFileContainer.fileComponents.push(file._id)
+       await root.freeFileContainer.save()
+    }else{
+        root.paidFileContainer.fileComponents.push(file._id)
+       await root.paidFileContainer.save()
+    }
+
+    return root.save()
+}
+
 
 exports.createFile = async (originalname, buffer, size, rootId, parentFolderId,userId) => {
 
+    const root = await rootModel.findById(rootId).populate("fileComponents")
 
-    const folder = await folderModel.findById(parentFolderId).populate("fileComponents") || await rootModel.findById(parentFolderId).populate("fileComponents")
+    let folder =  await folderModel.findById(parentFolderId).populate("fileComponents")
+
+    if(!folder) folder = root
+    checkIfStorageHaveEnoughtSpace(root,undefined,size)
+
     if (folder.fileComponents.some(el => el.fileName === originalname)) {
         throw new Error("File with the same name already exists.")
     }
@@ -166,11 +211,12 @@ exports.createFile = async (originalname, buffer, size, rootId, parentFolderId,u
         userId
     })
 
+    await putInFileContainer(newFile,root)
+
     folder.fileComponents.push(newFile._id)
     await folder.save()
-    const root = await rootModel.findById(rootId)
     root.usedStorage+=newFile.length
-    await root.save()
+    // await root.save()
     return newFile
 }
 
@@ -180,13 +226,15 @@ exports.addBytesToStorage= async(rootId,Bytes)=>{
     root.usedStorage+=Bytes
     await root.save()
 }
-exports.checkIfStorageHaveEnoughtSpace= async(rootId,Bytes)=>{
-    const root = await rootModel.findById(rootId)
+checkIfStorageHaveEnoughtSpace= async(root,rootId,Bytes)=>{
+    if(!root)  root = await rootModel.findById(rootId)
+
     if(root.usedStorage+Bytes>root.storageVolume){
-        throw new Error("There is no enought space")
+        throw new Error("There is no enough space")
     }
     
 }
+exports.checkIfStorageHaveEnoughtSpace = checkIfStorageHaveEnoughtSpace
 exports.createFolder = async (name, rootId, parentFolderId) => {
 
     const ownerId = (await userModel.findOne({ rootId }))._id
@@ -256,8 +304,7 @@ exports.getAllParentAutorisedFolders = async (folderId, userId) => {
 }
 
 exports.getOnlyRootInfo=async(rootId)=>{
-    const root = await rootModel.findById(rootId)
-    return root
+    return rootModel.findById(rootId)
 }
 exports.getTopFileExts=async(rootId)=>{
     let files = await fileModel.find({rootId})
@@ -362,3 +409,58 @@ exports.getSharedWithMeFolders = async (userId)=>{
 }
 
 
+exports.downloadFile=async(fileId,res)=>{
+    const fileFromDB = await fileModel.findById(fileId)
+    const gridFile = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+        bucketName: 'files'
+    });
+
+    const downloadStream = gridFile.openDownloadStream(fileFromDB.fileChunks);
+
+    res.set({
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': `attachment; filename="${fileFromDB.fileName}.${fileFromDB.type}"`
+    });
+
+    downloadStream.pipe(res);
+}
+
+
+exports.downloadFolder=async (folderId,res)=>{
+    const zip = archiver('zip', {
+        zlib: { level: 9 },
+    });
+
+
+    res.set({
+        'Content-Type': 'application/zip',
+        'Content-Disposition': `attachment; filename="zip.zip"`
+    });
+
+    zip.pipe(res)
+
+
+    await populateFolder([],folderId)
+    zip.finalize();
+}
+
+ async function populateFolder(folderTreeNames,folderId){
+    const folder = await folderModel.findById(folderId)
+    const updatedFolderTreeNames=[...folderTreeNames,folder.name]
+    const filePath = updatedFolderTreeNames.join("/")
+    zip.append(Buffer.alloc(0), { name:`${filePath}/` });
+    for (const fileId of folder.fileComponents) {
+        const file = await fileModel.findById(fileId)
+
+        const gridFile = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+            bucketName: 'files'
+        });
+        const downloadStream = gridFile.openDownloadStream(file.fileChunks);
+
+        zip.append(downloadStream, { name: `${filePath}/${file.fileName}.${file.type}` });
+    }
+    for (const nestedFolder of folder.dirComponents) {
+        await populateFolder(updatedFolderTreeNames,nestedFolder._id)
+    }
+
+}
