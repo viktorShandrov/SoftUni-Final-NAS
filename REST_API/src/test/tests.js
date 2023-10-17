b73d0f6ba618460b95077d265cb67fc806f2518a
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-17 14:25:15 +0300

    file compression tests

--- REST_API/src/test/tests.js
+++ REST_API/src/test/tests.js
@@ -1,74 +1,94 @@
 const fs = require("fs");
+const archiver = require('archiver');
 const zlib = require('zlib');
 const path = require("path");
 const async = require("async");
+const {call} = require("express");
+const {uploadFileToGC} = require("../managers/filesManager");
 
 exports.runTest=()=>{
     // gzipFile()
-    const inputFilePath = './output.gz'; // Replace with your input file path
-    const outputFilePath1 = './BATALIONA(2222).pptx'; // Replace with your output file path
-    const outputFilePath2 = './BATALIONA(33333).pptx'; // Replace with your output file path
+    // gunzip()
+    // uploadFileToGC()
 
-    const input = fs.createReadStream(path.join(__dirname, inputFilePath),{highWaterMark:1*1024*1024});
-    const output = fs.createWriteStream(path.join(__dirname, outputFilePath1));
-    const output2 = fs.createWriteStream(path.join(__dirname, outputFilePath2));
+}
+function gunzip(){
+    const inputFilePath = './output.zip'; // Replace with the path to your compressed file
+    const outputDirectory = './output1.gif'; // Replace with the path where you want to decompress the file
 
+    // Create a read stream from the compressed file
+    const input = fs.createReadStream(path.join(__dirname, inputFilePath));
 
-
+    // Create an async queue to handle parallel processing
     const queue = async.queue((chunk, callback) => {
-        // chunk = '1f8b080000000000000a' + chunk
-        zlib.gunzip(chunk, (err, decompressedChunk) => {
-            if (err) {
-                console.error('Error decompressing chunk:', err);
-                callback();
-            } else {
-                // Append the decompressed chunk to the output file
-                output.write(decompressedChunk, () => {
-                    callback();
-                });
-            }
+        const gunzip = zlib.createGunzip();
+        const output = fs.createWriteStream(path.join(__dirname, outputDirectory), { flags: 'a' }); // Append to the output file
+
+        // Pipe the chunk through the Gunzip stream and then to the output file
+        output.write(chunk);
+
+        // Close the output file and call the callback
+        output.on('finish', () => {
+            output.close();
+            callback();
         });
-    }, 4);
+    }, 4); // Number of parallel processes
 
+    // Read and process the file in chunks
+    let chunk;
+    let buffer = Buffer.alloc(0);
 
+    input.on('data', (data) => {
+        buffer = Buffer.concat([buffer, data]);
 
-    input.on("data",(chunk)=>{
-        queue.push(chunk)
+        while (buffer.length >= 1024 * 1024) { // Adjust the chunk size as needed
+            chunk = buffer.slice(0, 1024 * 1024);
+            buffer = buffer.slice(1024 * 1024);
 
-    })
-
-    input.pipe(zlib.createGzip()).pipe(output2);
-
-// Pipe the input stream through the decompressor and then to the output stream
-
-
-//     input.pipe(decompressor).pipe(output);
-    output.on('finish', () => {
-        console.log('queue complete.');
-        console.log(new Date().getTime())
+            queue.push(chunk);
+        }
     });
-    output2.on('finish', () => {
 
-        console.log('raw complete.');
-        console.log(new Date().getTime())
+    // Handle the end of the input stream
+    input.on('end', () => {
+        // Push the remaining data as the last chunk
+        if (buffer.length > 0) {
+            queue.push(buffer);
+        }
+
+        // When all tasks are completed, the processing is done
+        queue.drain(() => {
+            console.log('File decompressed successfully.');
+        });
     });
 }
 
 function gzipFile(){
-    const inputFilePath = './BATALIONA.pptx'; // Replace with your input file path
-    const outputFilePath = './output.gz'; // Replace with your output file path
+    const inputFilePath = 'standard.gif'; // Replace with your input file path
+    const outputFilePath = 'output.zip'; // Replace with your output file path
 
-// Create a readable stream from the input file
-    const input = fs.createReadStream(path.join(__dirname, inputFilePath));
-
-// Create a writable stream to the output file, gzipping the data
+// Create an output stream to write the compressed file
     const output = fs.createWriteStream(path.join(__dirname, outputFilePath));
 
-// Pipe the input through the gzip stream to the output
-    input.pipe(zlib.createGzip()).pipe(output);
-
-// Handle the completion of the gzip process
-    output.on('finish', () => {
-        console.log('File successfully gzipped.');
+// Create a new archiver instance
+    const archive = archiver('zip', {
+        zlib: { level: 9 }, // Compression level
     });
+
+// Pipe the output stream to the archive
+    archive.pipe(output);
+
+// Read the content of the input file
+    const fileContent = fs.readFileSync(path.join(__dirname, inputFilePath));
+
+// Add the file content to the archive
+    archive.append(fileContent, { name: 'file.gif' });
+
+// Finalize the archive, which will write the compressed file
+    archive.finalize();
+
+    output.on('close', () => {
+        console.log('File compressed and saved to output.zip.');
+    });
+
 }
