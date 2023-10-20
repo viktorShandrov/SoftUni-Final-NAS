d2f0bc20847430f7c6d979f95fc8e233fe46f64b
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-20 12:19:59 +0300

    mail html

--- REST_API/src/managers/usersManager.js
+++ REST_API/src/managers/usersManager.js
@@ -7,6 +7,7 @@ const {OAuth2Client} = require('google-auth-library');
 const {googleClientId} = require('../utils/utils');
 const notificationModel = require("../models/notificationModel")
 const { default: mongoose, trusted } = require("mongoose")
+const nodemailer = require('nodemailer');
 
 exports.register =async (email,password,repeatePassword,rootId,userId)=>{
     const user =await userModel.findOne({email})
@@ -49,7 +50,7 @@ exports.login = async (email,password,isLoggingFromGoogle)=>{
             }
             const token = await jwt.sign(payload,jwt.secret)
             
-        return {token,rootId:user.rootId,userId:user._id}
+        return {token,rootId:user.rootId,userId:user._id,email}
        }
        else{
         throw new Error("No such user or password is incorrect");
@@ -98,5 +99,89 @@ exports.areThereUnSeenNotifications=async(userId)=>{
         }
     }
     return false
+}
+exports.sendConfirmationEmail= async (email,password)=>{
+
+    const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Confirmation</title>
+</head>
+<body>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+        <tr>
+            <td align="center" bgcolor="#007BFF" style="padding: 40px 0 30px 0;">
+                <h1 style="color: #ffffff; font-size: 24px; font-weight: bold; margin: 0;">Email Confirmation</h1>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#f4f4f4" style="padding: 40px 30px 40px 30px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+                    <tr>
+                        <td style="color: #333333; font-size: 16px; line-height: 24px;">
+                            <p>Thank you for signing up! To complete your registration in "The Confederacy Cloud", please click the button below:</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;">
+                                <tr>
+                                    <td align="center" bgcolor="#007BFF" style="border-radius: 3px;">
+                                        <a href="http://localhost:4200/#/accountConfirmation/${email}/${password}" target="_blank" style="color: #ffffff; text-decoration: none; display: inline-block; padding: 10px 20px; font-size: 18px; font-weight: bold;">Confirm Your Email</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="color: #333333; font-size: 16px; line-height: 24px; padding-top: 20px;">
+                            <p>If you did not register on our website, please ignore this email.</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#007BFF" style="padding: 30px 30px 30px 30px;">
+                <p style="color: #ffffff; font-size: 12px; line-height: 18px; margin: 0;">&copy; 2023 The Confederacy. All rights reserved.</p>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>`
+
+// Create a Nodemailer transporter using Gmail SMTP
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.abv.bg',
+        port: 465, // Use the appropriate port for secure SMTP (abv.bg uses port 465 for SSL/TLS)
+        secure: true, // Use SSL/TLS
+        auth: {
+            user: 'viktor_shandrov@abv.bg', // Your abv.bg email address
+            pass: '#69BGshadopest43', // Your abv.bg email password
+        },
+        tls: {
+            rejectUnauthorized: false, // Add this to accept self-signed certificates
+        },
+    });
+
+// Email data
+    const mailOptions = {
+        from: 'viktor_shandrov@abv.bg',
+        to:email,
+        subject: 'Confirmation email',
+        html,
+    };
+
+// Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Error:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+
 }
 
