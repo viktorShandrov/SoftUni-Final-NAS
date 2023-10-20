74408f79b78814dc45b60f7929d160ae5c675fcc
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-20 12:18:20 +0300

    subscription stripe

--- REST_API/src/controllers/stripeController.js
+++ REST_API/src/controllers/stripeController.js
@@ -1,7 +1,8 @@
 const express = require("express")
-const {stripeSecret, stripeSecretKey} = require("../utils/utils");
-const stripe = require('stripe')(stripeSecretKey);
+const utils = require("../utils/utils");
+const stripe = require('stripe')(utils.stripeSecretKey);
 const filesManager = require("../managers/filesManager")
+
 const router = express.Router()
 
 router.use(express.raw({ type: 'application/json' }))
@@ -10,7 +11,7 @@ router.post("/paymentMade",async (req,res)=>{
     try {
 
         const sig = req.headers['stripe-signature'];
-        const event = stripe.webhooks.constructEvent(req.body, sig, stripeSecret);
+        const event = stripe.webhooks.constructEvent(req.body, sig, utils.stripeSecret);
         //checkout.session.completed
         if (event.type === 'payment_intent.succeeded') {
 
@@ -31,23 +32,22 @@ router.post('/create-checkout-session', async (req, res) => {
     try {
 
         const session = await stripe.checkout.sessions.create({
+            mode: 'subscription',
             payment_method_types: ['card'],
             line_items: [
                 {
-                    price_data: {
-                        currency: 'usd',
-                        product_data: {
-                            name: 'T-shirt',
-                        },
-                        unit_amount: 2000,
-                    },
+                    price: "price_1O3DfUHWjRJobyftOq1PQGlj",
+                    // For metered billing, do not pass quantity
                     quantity: 1,
                 },
             ],
-            mode: 'payment',
-            success_url: 'https://example.com/success',
-            cancel_url: 'https://example.com/cancel',
+            // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+            // the actual Session ID is returned in the query parameter when your customer
+            // is redirected to the success page.
+            success_url: `${constants.FEdomain}/#/storage/(storage-outlet:dashboard)`,
+            cancel_url: `${constants.FEdomain}/#/storage/(storage-outlet:dashboard)`,
         });
+
 
         res.json({ id: session.id });
 
